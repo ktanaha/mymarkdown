372b7182e97eab65a18987358fba4f11fd8b1bb7
ktanaha <moutoku1984@gmail.com>
2018-05-05 20:20:34 +0900

    add memo list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="home">
     <h1>{{ msg }} </h1>
-    <button @click="googleLogin">Google アカウントでログイン</button>
+    <button @click="googleLogin">
+      Google アカウントでログイン
+    </button>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'home',
-    data () {
-      return {
-        msg: 'Welcome to MyMarkDown'
-      }
-    },
-    methods: {
-      googleLogin: function() {
-        firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-      }
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to MyMarkDown'
+    }
+  },
+  methods: {
+    googleLogin: function() {
+      firebase.auth().signInWithRedirect(
+        new firebase.auth.GoogleAuthProvider());
     }
   }
+}
 </script>
